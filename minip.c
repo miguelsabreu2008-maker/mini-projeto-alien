a6cd290029387d17fadd34c0235076d2f86a1c05
miguelsabreu <miguelabreu@discente.ufg.br>
2026-09-25 16:14:42 -0300

    a

--- minip.c
+++ minip.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
+
+
+
+
 int main() {
+    char str[10001];
+    int n[20];
+
+    scanf("%s", str);
+
+    while(1) {
+        scanf("%d", n);
+        if(n == 0 ) {
+            break;
+        }
+    }
+    printf("%ls", n);
 
     return 0;
 }
